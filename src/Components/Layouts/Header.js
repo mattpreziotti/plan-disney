db9f9dab0642f45280d830ab9842982e4da5a29c
Prez <matthew.preziotti@gmail.com>
2020-07-16 22:00:55 -0400

    updated header

--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,11 +1,47 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography } from '@material-ui/core'
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+} from "@material-ui/core";
+import MenuIcon from '@material-ui/icons/Menu';
 
-export default props =>
-    <AppBar position="static">
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
         <Toolbar>
-            <Typography variant="h6" color="inherit">
-                Exercise Database
-            </Typography>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Plan Disney
+          </Typography>
+          <Button color="inherit">Login</Button>
         </Toolbar>
-    </AppBar>
+      </AppBar>
+    </div>
+  );
+}
